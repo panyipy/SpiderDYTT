56607a3ec9aee7451831a543c16961c5085caa6f
Jayden <yancyxxx@163.com>
2017-11-25 01:48:37 +0800

    实现爬取电影名字和对应URL

--- dytt.py
+++ dytt.py
@@ -1,18 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getData():
-    url = "http://www.dytt8.net/"
-    userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-    headers = {'content-type': 'application/json',
-               'User-Agent': userAgent}
-    webData = requests.get(url=url, headers=headers).content
-    soup = BeautifulSoup(webData, 'html.parser')
-    movie_name = soup.select("div.co_content8 > ul > table > tbody > tr > td")
-    for name in movie_name:
-        data = {
-            'name': name.get_text()
-        }
-        print(data)
-
-getData()
+url = 'http://www.dytt8.net/'
+userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+headers = {'content-type': 'application/json',
+           'User-Agent': userAgent}
+webData = requests.get(url=url, headers=headers).content.decode('gbk')
+soup = BeautifulSoup(webData, 'html.parser')
+items_dict = {}
+movie_items = soup.select("div.co_content8 > ul > table > tr > td.inddline > a")
+for items in movie_items:
+    text = items.get_text()
+    if text != "最新电影下载":
+        print(text)
+        string = str(items)
+        a, url, content01 = string.split('"')
+        url = "http://www.dytt8.net{}".format(url)
+        print(url)
