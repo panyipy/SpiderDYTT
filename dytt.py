2ba7fc0adad769ccfb0a1a8f00030fd0b7656b75
Jayden <yancyxxx@163.com>
2017-11-25 02:32:52 +0800

    修改只获取最新电影的标题和URL，增加总计数

--- dytt.py
+++ dytt.py
@@ -7,13 +7,21 @@ headers = {'content-type': 'application/json',
            'User-Agent': userAgent}
 webData = requests.get(url=url, headers=headers).content.decode('gbk')
 soup = BeautifulSoup(webData, 'html.parser')
-items_dict = {}
-movie_items = soup.select("div.co_content8 > ul > table > tr > td.inddline > a")
-for items in movie_items:
-    text = items.get_text()
-    if text != "最新电影下载":
-        print(text)
-        string = str(items)
-        a, url, content01 = string.split('"')
-        url = "http://www.dytt8.net{}".format(url)
-        print(url)
+movie_items_tr = soup.select("div.co_content8 > ul > table > tr > td.inddline")
+# movie_items = soup.select("div.co_content8 > ul > table > tr > td.inddline > a")
+count_num = 0
+for items01 in movie_items_tr:
+    tr_str = str(items01)
+    movie_items = items01.select("a")
+    if "最新电影下载" in tr_str:
+        for items in movie_items:
+            text = items.get_text()
+            if text != "最新电影下载":
+                print(text)
+                string = str(items)
+                a, url, content01 = string.split('"')
+                url = "http://www.dytt8.net{}".format(url)
+                print(url)
+                count_num = count_num + 1
+count_str = "共{}条数据" .format(count_num)
+print(count_str)
