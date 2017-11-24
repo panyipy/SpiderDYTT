ba91c524addc40c3c8c10693dfb790726daee21d
Jayden <yancyxxx@163.com>
2017-11-24 18:47:07 +0800

    完善代码

--- dytt.py
+++ dytt.py
@@ -6,8 +6,13 @@ def getData():
     userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
     headers = {'content-type': 'application/json',
                'User-Agent': userAgent}
-    webData = requests.get(url=url, headers=headers).content.decode('gbk')
-    soup = BeautifulSoup(webData, 'html.paraser')
-    #修改试试
+    webData = requests.get(url=url, headers=headers).content
+    soup = BeautifulSoup(webData, 'html.parser')
+    movie_name = soup.select("div.co_content8 > ul > table > tbody > tr > td")
+    for name in movie_name:
+        data = {
+            'name': name.get_text()
+        }
+        print(data)
 
 getData()
